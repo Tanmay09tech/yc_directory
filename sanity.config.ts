0c695b87da56d50e10bdbe028ad87656cc7f68e1
Tanmay Basak <tanmaybasak2071@gmail.com>
2025-01-10 20:47:46 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,22 +8,25 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {apiVersion, dataset, projectId} from './sanity/env'
+
 import {schemaTypes} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
+
 import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-
   // Add and edit the content schema in the './sanity/schemaTypes' folder
+  
   schema: {
     types: schemaTypes,
   },
   plugins: [
     structureTool({structure}),
     visionTool({defaultApiVersion: apiVersion}),
+
     markdownSchema(),
   ],
 })
